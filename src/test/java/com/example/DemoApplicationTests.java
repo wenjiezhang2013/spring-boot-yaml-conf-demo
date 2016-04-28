0b16ee6125fb90d87cf6f1ad3a966a64b754ec9c
Wenjie ("Jack") Zhang <wenjie_zhang@apple.com>
2016-04-28 11:04:41 -0700

    Proof that "ActiveProfiles" annotation does have a precedence on the order manner

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -4,21 +4,24 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.example.config.DemoConfig;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = DemoApplication.class)
+@ActiveProfiles(
+                value = { "x", "unit-test" }) // "unit-test" will override "x" profile
 public class DemoApplicationTests {
-	
-	@Autowired
-	private DemoConfig demoConfig;
-	
 
-	@Test
-	public void contextLoads() {
-		org.junit.Assert.assertEquals("unit-test", demoConfig.getName());
-	}
+    @Autowired
+    private DemoConfig demoConfig;
+
+
+    @Test
+    public void contextLoads() {
+        org.junit.Assert.assertEquals("unit-test", demoConfig.getName());
+    }
 
 }
